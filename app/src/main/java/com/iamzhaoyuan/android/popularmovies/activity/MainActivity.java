bf026a0d56d8aa0a10f232c9b4b2dba9b9471c51
Yuan Zhao <yuan.zhao@nextlabs.com>
2016-12-06 16:45:55 +0800

    removed settings

--- app/src/main/java/com/iamzhaoyuan/android/popularmovies/activity/MainActivity.java
+++ app/src/main/java/com/iamzhaoyuan/android/popularmovies/activity/MainActivity.java
@@ -40,25 +40,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(
